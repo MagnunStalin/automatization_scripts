a13eba38a6848ef51dc37c1053270bfbe470d67e
Stalin Villacis <stalinvillacis@me.com>
2024-07-08 14:48:58 -0500

    Copy multiple files an directories to external unit.

--- scripts/backups/backup_directory.sh
+++ scripts/backups/backup_directory.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
-#autor:Pasantes
-#Script de automatización para respaldo de archivos adjuntos de fulltime web.
-#These functions return exit codes: 0 = found, 1 = not found
+# Author: Pasantes & Stalin Villacis
+# Copy multiples files and directories to external unit
 
+# These functions return exit codes: 0 = found, 1 = not found
 isDevMounted () { findmnt --source "$1" >/dev/null;} #device only
 isPathMounted() { findmnt --target "$1" >/dev/null;} #path   only
 isMounted    () { findmnt          "$1" >/dev/null;} #device or path
 
 
-directory="/dir1/dir2/dir3"
-punto_montaje="/mnt/mount_point_dir"
-NETWORK_BACKUP="//external_device/dir1/dir2/dir3"
-USERNAME="username"
-PASSWORD="passwd"
+DIRECTORIES=("/dir1" "/dir2/dir2_a" "/dir2/dir_b" "dir3/file1")
+MOUNT_POINT="/mnt/point_mount"
+NETWORK_BACKUP="//network_unit/dir1/dir2"
+USERNAME="user"
+PASSWORD="password"
 DOMAIN="local.local"
 
 set -e
-#1.Verificar que el punto de montaje se encuentre creado
-if [ ! -d "$punto_montaje" ]; then
-    mkdir $punto_montaje
+# 1. Verify if the mount point is created
+if [ ! -d "$MOUNT_POINT" ]; then
+    mkdir $MOUNT_POINT
 fi
-#2.Montar la unidad de red
-mount -t cifs "$NETWORK_BACKUP"  $punto_montaje -o username=$USERNAME,password=$PASSWORD,domain=$DOMAIN
 
-#3.Verificar si la unidad de red esta montada
-if  isDevMounted "$NETWORK_BACKUP";
-   then echo "device is mounted"
-   else 
+# 2. Mount netowrk unit
+mount -t cifs "$NETWORK_BACKUP"  $MOUNT_POINT -o username=$USERNAME,password=$PASSWORD,domain=$DOMAIN
+
+# 3.Verify is the network unit is mounted
+if  isDevMounted "$NETWORK_BACKUP"; then 
+    echo "device is mounted"
+else 
    echo "device is not mounted"
    exit 1 
 fi
 
-#4.Copiar los respaldos
-rsync -rvz "$directory" "$punto_montaje"/
+# 4. Copy directories
+for DIR in "${DIRECTORIES[@]}"; do
+    if [ -d "$DIR" ]; then
+        rsync -rvz "$DIR" "$MOUNT_POINT"/
+    else
+        rsync -vz "$DIR" "$MOUNT_POINT"/
+    fi
+done
 
 #5.Desmontar la unidad de red
-umount "$punto_montaje"
+umount "$MOUNT_POINT"
